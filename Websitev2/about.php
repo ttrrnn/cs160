1089799a3ec581bf278a966028d7b162a740db3d
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-04-25 10:37:37 -0700

    Create about.php

---
+++ Websitev2/about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="DataTables-1.9.4/media/css/jquery.dataTables.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/jquery-2.1.0.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="DataTables-1.9.4/media/js/jquery.dataTables.min.js"></script>
+    <script src="js/application.js" type="text/javascript"></script>
+    <title>Educademy About Us</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php include('includes/header.php'); ?>
+        <?php include('includes/nav.php'); ?>
+        <div id="content">
+            <h3>About Us</h3>
+            <p>We are the Course Cookers, and we are ready to serve up some excellent courses.</p>
+        </div> <!-- end #content -->
+        <?php include('includes/footer.php'); ?>
+    </div> <!-- End #wrapper -->
+</body>
+
+</html>
