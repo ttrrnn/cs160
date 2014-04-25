e05d7bf22e4d28d2a2d22916e7aa759a5a4e1572
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-04-25 10:40:18 -0700

    Create index.php

---
+++ Websitev2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="DataTables-1.9.4/media/css/jquery.dataTables.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="js/jquery-2.1.0.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="DataTables-1.9.4/media/js/jquery.dataTables.min.js"></script>
+    <script src="js/application.js" type="text/javascript"></script>
+    <title>Educademy</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <?php include('includes/header.php'); ?>
+        <?php include('includes/nav.php'); ?>
+        <div id="content">
+            <h1>Welcome!</h1>
+            <p>Some comforting words here.</p>
+        </div> <!-- end #content -->
+        <?php include('includes/footer.php'); ?>
+    </div> <!-- End #wrapper -->
+</body>
+    
+</html>
