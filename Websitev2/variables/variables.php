1585b5986999dd6fe5e453077d8caf91a5ba599c
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-04-29 13:03:06 -0700

    Added connection info

--- Websitev2/variables/variables.php
+++ Websitev2/variables/variables.php
@@ -2,5 +2,9 @@
 
 $heading='Educademy';
 $footer='Copyright &copy; 2014 Course Cookers';
+$db_user='sjsucsor_s5g414s';
+$db_password='N0VACITY';
+$db_name='sjsucsor_160s5g42014s';
+$db_host='127.0.0.1';
 
 ?>
