c2e5e7e4baa6fbc4dd52af4b0eee04e599c28f69
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-04-30 08:44:51 -0700

    Added a function to determine active element

--- Websitev2/includes/nav.php
+++ Websitev2/includes/nav.php
@@ -1,5 +1,17 @@
 <ul class="nav nav-tabs">
-    <li class="active"><a href="index.php">Home</a></li>
-    <li><a href="results.php">Courses</a></li>
-    <li><a href="about.php">About</a></li>	
+    <li <?=echoActiveClass("index")?>><a href="index.php">Home</a></li>
+    <li <?=echoActiveClass("results")?>><a href="results.php">Courses</a></li>
+    <li <?=echoActiveClass("about")?>><a href="about.php">About</a></li>	
 </ul> <!-- end nav -->
+
+<?php 
+
+function echoActiveClass($requestUri)
+{
+    $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+
+    if ($current_file_name == $requestUri)
+        echo 'class="active"';
+}
+
+?>
