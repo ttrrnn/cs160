b0e6fac8a96d4cb2deeb9da6be7cc0469568e709
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-02 06:52:49 -0700

    Create application.js

---
+++ Websitev3/js/application.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#table").dataTable ( {
+        "bProcessing": true,
+        "sPaginationType": "full_numbers",
+        "aoColumns": [
+        {
+            "bSortable": false, 
+            "bSearchable": false
+        },
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+        ]
+    });
+
+    $.get("get_courses.php", function(result) {
+        $("#table").dataTable().fnAddData(JSON.parse(result));
+    });
+});
