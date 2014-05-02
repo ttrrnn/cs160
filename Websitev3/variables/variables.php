7e1a51f4a9a2e6dde574336decb18867dead5067
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-02 06:54:36 -0700

    Create variables.php

---
+++ Websitev3/variables/variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$heading='Educademy';
+$footer='Copyright &copy; 2014 Course Cookers';
+$db_user='sjsucsor_s5g414s';
+$db_password='N0VACITY';
+$db_name='sjsucsor_160s5g42014s';
+$db_host='127.0.0.1';
+
+?>
