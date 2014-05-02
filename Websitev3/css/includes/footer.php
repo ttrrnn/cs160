ba77642abb26b27b3df66cc0637a0f5540c71361
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-02 06:50:00 -0700

    Delete footer.php

--- Websitev3/css/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<?php include('variables/variables.php'); ?>
-
-<div class="container">
-    <hr>
-    <p class="pull-right">
-        <a href="#">Back to top</a>
-    </p>
-    <p><?php echo $footer ?></p>
-</div>
