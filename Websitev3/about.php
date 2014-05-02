52945b442b8157d95b8f0b1fc96fbb9311033bcb
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-02 06:42:18 -0700

    Create about.php

---
+++ Websitev3/about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+        <link href="css/style.css" type="text/css" rel="stylesheet">
+        <title>Educademy About Us</title>
+    </head>
+    <body>
+        <?php include('includes/header.php'); ?>
+        <div class="container">
+            <h1>About Us</h1>
+            <p>We are the Course Cookers, and we are ready to serve up some excellent courses.</p>
+            <img src="images/2JZkmtH.gif">
+        </div>
+        <?php include('includes/footer.php'); ?>
+        <!-- JS placed at the end of the document so the page loads faster -->
+        <script src="js/jquery-2.1.0.min.js"></script>
+        <script src="js/jquery.validate.min.js"></script>
+        <script src="js/application.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
