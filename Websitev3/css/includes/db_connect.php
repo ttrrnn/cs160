329a0273b5245a3a05d0f9a28e56324737371680
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-02 06:50:07 -0700

    Delete db_connect.php

--- Websitev3/css/includes/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php include('variables/variables.php');
-    $db = new mysqli($db_host, $db_user, $db_password, $db_name);
-    if ($db->connect_errno) {
-        echo "Failed to connect to MySQL: " . $db->connect_error;
-        exit();
-    }
-?>
