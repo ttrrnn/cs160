665269caef203a70a0f212ed755b975280654652
trannosaurus <vtran.sj@gmail.com>
2014-05-02 13:40:41 -0700

    updated about page

--- Websitev3/about.php
+++ Websitev3/about.php
@@ -14,9 +14,16 @@
     <body>
         <?php include('includes/header.php'); ?>
         <div class="container">
-            <h1>About Us</h1>
-            <p>We are the Course Cookers, and we are ready to serve up some excellent courses.</p>
-            <img src="images/2JZkmtH.gif">
+				<h1>About Us</h1>
+				<h2>Who We Are</h2>
+				<p>We are the Course Cookers, and we are ready to serve up some excellent courses.</p>
+				<h2>What We Do</h2>
+				<p>Are you someone who is constantly hungry to learn and gain more knowledge? Always up for a new challenge and learning something new? Well then, Educademy is the right place to be!</p>
+				<p>Treat Educademy as a buffet where you aren't limited to how much knowledge you can consume! Here at Educademy, you work indirectly with the chefs who will cater to your needs and help you pile online courses on your plate in order to feed that hunger for learning whatever it is you are seeking to learn.</p>
+				<p>Educademy aims to make your life simpler by helping you search for education online courses from 8 different massive open online courses (MOOC) so you don't have to visit all 8 sites to discover from which site you want to take the particular course.</p>
+				<p>Our talent chefs here will get right to work using ingredients gathered from the 8 MOOCs when you start typing in the search box on the Courses page. As we strive to provide you with the highest quality of MOOCs, our chosen 8 MOOCs are, in no particular order, Canvas Network, FutureLearn, Open2Study, Coursera, Iversity, Udacity, EDX, and NovoEd.</p>
+				<p>From here on out, the buffet of courses are yours! We are confident that the courses you have chosen for enrollment will satisfy your hunger for a particular knowledge, if not it may have piqued your interest even more! If you aren't satisfied, have no fear, Educademy is a buffet; meaning, you can feast for as long as you please. Happy learning, and we wish you the best!</p>
+				<img src="images/2JZkmtH.gif">
         </div>
         <?php include('includes/footer.php'); ?>
         <!-- JS placed at the end of the document so the page loads faster -->
