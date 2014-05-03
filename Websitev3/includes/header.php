6dee4882b6b82735d03184a6d01a7b134d2a0b69
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-03 00:13:44 -0700

    Added a generic modal for future use

--- Websitev3/includes/header.php
+++ Websitev3/includes/header.php
@@ -66,3 +66,17 @@ function echoActiveClass($requestUri) {
         </div>
     </div>          
 </div>
+<div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                <h4 class="modal-title" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
