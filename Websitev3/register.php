66b741fef49f6031eca5deb57d640af7e878c2fc
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-03 15:38:27 -0700

    Fixed password hashing

--- Websitev3/register.php
+++ Websitev3/register.php
@@ -100,9 +100,6 @@ if (!empty($_POST)) {
     // Security measures
     $salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
     $password = hash('sha256', $_POST['password'] . $salt);
-    for ($round = 0; $round < 65536; $round++) {
-        $password = hash('sha256', $password . $salt);
-    }
     $query_params = array(
         ':username' => $_POST['username'],
         ':password' => $password,
