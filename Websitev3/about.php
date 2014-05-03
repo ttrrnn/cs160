14a89787b928d74b8be31123f77d07721ffa20ae
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-03 07:53:24 -0700

    Tran changes

--- Websitev3/about.php
+++ Websitev3/about.php
@@ -14,13 +14,14 @@
     <body>
         <?php include('includes/header.php'); ?>
         <div class="container">
-	    <hr class="featurette-divider">
+            <h1>About Us</h1>
+            <hr class="featurette-divider">
             <div class="row featurette">
                 <div class="col-md-5">
-                    <img src="images/2JZkmtH.gif">
+                    <img class="thumbnail" src="images/2JZkmtH.gif">
                 </div>
                 <div class="col-md-7">
-                    <h2 class="featurette-heading">Who We Are. <span class="text-muted">It'll blow your mind.</span></h2>
+                    <h2 class="featurette-heading">Who We Are. <span class="text-muted">It will blow your mind.</span></h2>
                     <p class="lead">We are the Course Cookers, and we are ready to serve up some excellent courses.</p>
                 </div>
             </div>
@@ -30,12 +31,23 @@
                     <h2 class="featurette-heading">What We Do. <span class="text-muted">See for yourself.</span></h2>
                     <p class="lead">Are you someone who is constantly hungry to learn and gain more knowledge? Always up for a new challenge and learning something new? Well then, <code>Educademy</code> is the right place to be!</p>
                     <p>Treat <code>Educademy</code> as a buffet where you aren't limited to how much knowledge you can consume! Here at <code>Educademy</code>, you work indirectly with the chefs who will cater to your needs and help you pile online courses on your plate in order to feed that hunger for learning whatever it is you are seeking to learn.</p>
-                    <p><code>Educademy</code> aims to make your life simpler by helping you search for education online courses from 8 different <abbr title="Massive Open Online Course">MOOC</abbr> so you don't have to visit all 8 sites to discover from which site you want to take the particular course.</p>
-                    <p>Our talent chefs here will get right to work using ingredients gathered from the 8 <abbr title="Massive Open Online Course">MOOC</abbr>s when you start typing in the search box on the Courses page. As we strive to provide you with the highest quality of <abbr title="Massive Open Online Course">MOOC</abbr>s, our chosen 8 <abbr title="Massive Open Online Course">MOOC</abbr>s are, in no particular order, Canvas Network, FutureLearn, Open2Study, Coursera, Iversity, Udacity, EDX, and NovoEd.</p>
-                    <p>From here on out, the buffet of courses are yours! We are confident that the courses you have chosen for enrollment will satisfy your hunger for a particular knowledge, if not it may have piqued your interest even more! If you aren't satisfied, have no fear, <code>Educademy</code> is a buffet; meaning, you can feast for as long as you please. Happy learning, and we wish you the best!</p>
+                    <p><code>Educademy</code> aims to make your life simpler by helping you search for educational online courses from 8 different <abbr title="Massive Open Online Course">MOOC</abbr>s so you don't have to visit all 8 sites to discover from which site you want to take the particular course.</p>
                 </div>
                 <div class="col-md-5">
-                    <!-- need image -->
+                    <img class="thumbnail" src="images/whoacatfood.gif">
+                </div>
+            </div>
+            <hr class="featurette-divider">
+            <div class="row featurette">
+                <div class="col-md-5">
+                    <img class="thumbnail" src="images/nekocatsohappy.gif">
+                </div>
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">How We Do. <span class="text-muted">The secret ingredient.</span></h2>
+                    <p>When you start typing in the search box on the <a href="courses.php">Courses</a> page, our talented chefs will get right to work using ingredients gathered from the 8 <abbr title="Massive Open Online Course">MOOC</abbr>s.</p>
+                    <p>As we strive to provide you with the highest quality of <abbr title="Massive Open Online Course">MOOC</abbr>s, our chosen 8 <abbr title="Massive Open Online Course">MOOC</abbr>s are Canvas Network, FutureLearn, Open2Study, Coursera, Iversity, Udacity, EDX, and NovoEd.</p>
+                    <p>From here on out, the buffet of courses are yours! We are confident that the courses you have chosen for enrollment will satisfy your hunger for a particular knowledge, if not it may have piqued your interest even more! If you aren't satisfied, have no fear, <code>Educademy</code> is a buffet; meaning, you can feast for as long as you please.</p>
+                    <p><h3> Happy learning! We wish you the best!</h3></p>
                 </div>
             </div>
         </div>
