b0dbe7808514a2ba59a7c00372f17cdd664262c8
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-03 22:26:52 -0700

    Added back-to-top script

--- Websitev3/includes/footer.php
+++ Websitev3/includes/footer.php
@@ -3,7 +3,7 @@
 <div class="container">
     <hr>
     <p class="pull-right">
-        <a href="#">Back to top</a>
+        <a class="back-to_top" href="#">Back to top</a>
     </p>
     <p><?php echo $footer ?></p>
 </div>
