6d1c6202ba07316161c1cc50c43f9b30914c227a
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-05 08:16:35 -0700

    Combining code

--- Websitev3/js/application.js
+++ Websitev3/js/application.js
@@ -1,32 +1,80 @@
 $(document).ready(function() {
+    // Fancybox
+    $(".fancybox").fancybox();
+    
+    // Validation
+    $("#form").validate(
+    {
+        rules: {
+            username: {
+                minlength: 2,
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                minlength: 6,
+                required: true
+            }
+        },
+        highlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+        }
+    });
+                
+    // Back-to-top
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({
+            scrollTop: 0
+        }, duration);
+        return false;
+    })
+    
+    // Datatables
     $("#table").dataTable ( {
         "bDeferRender": true,
         "sPaginationType": "full_numbers",
         "aoColumns": [
-            {
-                "bSortable": false, 
-                "bSearchable": false
-            },
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null
+        {
+            "bSortable": false, 
+            "bSearchable": false
+        },
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
         ],
         "fnDrawCallback": function (oSettings) {
             // Need code here to conditionally check if it hasn't already
             // been initialised on a particular .raty element
             $('.raty').raty({
-                score    : function() {
+                score: function() {
                     return $(this).attr("value");
                 },
             
-                readOnly : true,
-                starHalf : 'images/star-half.png',
-                starOff  : 'images/star-off.png',
-                starOn   : 'images/star-on.png'
+                readOnly    : true,
+                starHalf    : 'images/star-half.png',
+                starOff     : 'images/star-off.png',
+                starOn      : 'images/star-on.png'
             });
         }
     });
