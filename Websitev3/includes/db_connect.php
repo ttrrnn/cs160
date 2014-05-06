409807938b153110bc4faf0e1f25dcd2ad89f598
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-06 14:14:46 -0700

    Fixed character encoding problems

--- Websitev3/includes/db_connect.php
+++ Websitev3/includes/db_connect.php
@@ -1,5 +1,7 @@
 <?php include('variables/variables.php');
     $db = new mysqli($db_host, $db_user, $db_password, $db_name);
+    $db->set_charset("utf8");
+    
     if ($db->connect_errno) {
         echo "Failed to connect to MySQL: " . $db->connect_error;
         exit();
