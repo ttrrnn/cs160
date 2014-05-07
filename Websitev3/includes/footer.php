c852d928940101ccd6f189903cf87b3b3bf47257
Pixelsheep <Pixelsheep@users.noreply.github.com>
2014-05-07 10:30:49 -0700

    Links to sjsu and sjsu-cs as per final doc

--- Websitev3/includes/footer.php
+++ Websitev3/includes/footer.php
@@ -5,5 +5,5 @@
     <p class="pull-right">
         <a class="back-to-top" href="#">Back to top</a>
     </p>
-    <p><?php echo $footer ?></p>
+    <p><strong><?php echo $footer ?></strong>   <a href="http://www.sjsu.edu/"><img src="images/sjsu_logo.jpg"></a><a href="http://www.sjsu.edu/cs/"><img src="images/sjsu_cs_logo.jpg"></a></p>
 </div>
